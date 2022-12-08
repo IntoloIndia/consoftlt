762330fc1bade127f84c48c5d1fbdd04d908744f
Intoloshubham <shubham.sahu@intoloindia.com>
2022-12-08 15:22:02 +0530

    "lt cmt 1st "

--- screens/UserScreens/UserReports/ReportApi.js
+++ screens/UserScreens/UserReports/ReportApi.js
@@ -74,6 +74,43 @@ const save_new_equipment_item = async data => {
   }
 };
 
+const send_otp_verification = async (email)=>{
+  try {
+    const res = fetch(`${process.env.API_URL}forget-password`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(email)
+    });
+    return res;
+  } catch (error) {
+    console.log(error)
+  }
+}
+const verify_password_otp = async (id,inputs)=>{
+  try {
+    const res = fetch(`${process.env.API_URL}verify-otp/${id}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(inputs)
+    });
+    return res;
+  } catch (error) {
+    console.log(error)
+  }
+}
+const reset_password = async (user_Id,inputs)=>{
+  try {
+    const res = fetch(`${process.env.API_URL}reset-password/${user_Id}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(inputs)
+    });
+    return res;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const insert_manpower_report = async (manpower_post_data, CONST_FIELD) => {
   try {
     const res = fetch(`${process.env.API_URL}report/${CONST_FIELD.MANPOWER}`, {
@@ -501,5 +538,8 @@ export {
   update_voucher_detail,
   insert_voucher_details,
   get_projectBy_userId,
-  get_all_unit
+  get_all_unit,
+  send_otp_verification,
+  verify_password_otp,
+  reset_password
 };
